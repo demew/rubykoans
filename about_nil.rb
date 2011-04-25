5c27df8c6d62bb153d6b7f078064d0f837f118a4
Addie Beseda <addie@trimentation.com>
2011-04-25 02:29:16 -0700

    Initial commit, about halfway through

---
+++ about_nil.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/edgecase')
+
+class AboutNil < EdgeCase::Koan
+  def test_nil_is_an_object
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
+  end
+
+  def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
+    # What happens when you call a method that doesn't exist.  The
+    # following begin/rescue/end code block captures the exception and
+    # make some assertions about it.
+    begin
+      nil.some_method_nil_doesnt_know_about
+    rescue Exception => ex
+      # What exception has been caught?
+      assert_equal NoMethodError, ex.class
+
+      # What message was attached to the exception?
+      # (HINT: replace __ with part of the error message.)
+      assert_match(/nil/, ex.message)
+    end
+  end
+
+  def test_nil_has_a_few_methods_defined_on_it
+    assert_equal true, nil.nil? # all-object method that tests for nil
+    assert_equal '', nil.to_s # all-object method that returns string rep.
+    assert_equal 'nil', nil.inspect # all-object method that returns human-
+                                 # readable string rep
+
+    # THINK ABOUT IT:
+    #
+    # Is it better to use
+    #    obj.nil?
+    # or
+    #    obj == nil
+    # Why?
+  end
+
+end
